bd5739957bbe803690001556205d001966234fa0
PRADHISH <ss2903494@gmail.com>
2026-06-13 12:36:41 +0530

    Create courserecord.java

---
+++ courserecord.java
@@ -0,0 +1,11 @@
+public class day8 {
+    record CourseRecord(String EEE,String cs){
+
+
+    }
+    static void main() {
+        CourseRecord courserecord=new CourseRecord("EEE","CS");
+        System.out.println(courserecord.EEE);
+        System.out.println(courserecord.cs);
+    }
+}
